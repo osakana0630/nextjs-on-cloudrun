1622098bfae7cf931598db4ceb165d98ea1b6717
osakana0630 <osakana0630@gmail.com>
2024-03-15 17:31:48 +0900

    update5

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          CI/CDのテスト
+          CI/CDのテスト2
         </h1>
 
       </main>
