d4d442c82dd2458736fb0b7eecb239c013083803
osakana0630 <osakana0630@gmail.com>
2024-03-15 17:25:41 +0900

    update4

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Next.jsのアプリケーションをCloudRun上にデプロイ(update)
+          CI/CDのテスト
         </h1>
 
       </main>
